7e1f6d818a15d953c34312ade2c9e7d7d803cf72
yuchanns <airamusume@gmail.com>
2021-02-04 02:31:56 +0800

    chore: improve code of test case

--- src/value/pod.rs
+++ src/value/pod.rs
@@ -2,6 +2,8 @@ use crate::value::error::Error;
 use crate::value::number::Number;
 use std::collections::HashMap;
 
+type IResult<T> = Result<T, Error>;
+
 #[derive(Debug, Clone)]
 pub enum Pod {
     Null,
@@ -36,7 +38,7 @@ impl Pod {
     }
 
     /// Pushes a new value into `Pod::Array`.
-    pub fn push<T>(&mut self, value: T) -> Result<(), Error>
+    pub fn push<T>(&mut self, value: T) -> IResult<()>
     where
         T: Into<Pod>,
     {
@@ -58,7 +60,7 @@ impl Pod {
     }
 
     /// Inserts a key value pair into or override the exist one in Pod::Hash.
-    pub fn insert<T>(&mut self, key: &'static str, val: T) -> Result<(), Error>
+    pub fn insert<T>(&mut self, key: &'static str, val: T) -> IResult<()>
     where
         T: Into<Pod>,
     {
@@ -87,18 +89,20 @@ impl Pod {
 // todo: impl trait Into<T: Pod> for Number
 
 #[test]
-fn test_partial_compare_null() {
+fn test_partial_compare_null() -> std::result::Result<(), Error> {
     assert_eq!(true, Pod::Null == Pod::Null);
+    Ok(())
 }
 
 #[test]
-fn test_partial_compare_boolean() {
+fn test_partial_compare_boolean() -> std::result::Result<(), Error> {
     assert_eq!(true, Pod::Boolean(true) == Pod::Boolean(true));
     assert_eq!(false, Pod::Boolean(true) == Pod::Boolean(false));
+    Ok(())
 }
 
 #[test]
-fn test_partial_compare_string() {
+fn test_partial_compare_string() -> std::result::Result<(), Error> {
     assert_eq!(
         true,
         Pod::String("hello".into()) == Pod::String("hello".into())
@@ -107,80 +111,44 @@ fn test_partial_compare_string() {
         false,
         Pod::String("hello".into()) == Pod::String("world".into())
     );
+    Ok(())
 }
 
 #[test]
-fn test_partial_compare_array() {
+fn test_partial_compare_array() -> std::result::Result<(), Error> {
     let mut a = Pod::new_array();
     let mut b = a.clone();
     assert_eq!(true, a == b);
-    match a.push(Pod::Boolean(true)) {
-        Ok(_) => {}
-        Err(err) => panic!(err),
-    };
-    match b.push(Pod::Boolean(true)) {
-        Ok(_) => {}
-        Err(err) => panic!(err),
-    };
+    a.push(Pod::Boolean(true))?;
+    b.push(Pod::Boolean(true))?;
     assert_eq!(true, a == b);
-    match a.push(Pod::String("hello".into())) {
-        Ok(_) => {}
-        Err(err) => panic!(err),
-    };
-    match b.push(Pod::String("hello".into())) {
-        Ok(_) => {}
-        Err(err) => panic!(err),
-    };
+    a.push(Pod::String("hello".into()))?;
+    b.push(Pod::String("hello".into()))?;
     assert_eq!(true, a == b);
-    match a.push(Pod::String("world".into())) {
-        Ok(_) => {}
-        Err(err) => panic!(err),
-    };
-    match b.push(Pod::String("world!".into())) {
-        Ok(_) => {}
-        Err(err) => panic!(err),
-    };
+    a.push(Pod::String("world".into()))?;
+    b.push(Pod::String("world!".into()))?;
     assert_eq!(false, a == b);
+    Ok(())
 }
 
 #[test]
-fn test_partial_compare_hash() {
+fn test_partial_compare_hash() -> std::result::Result<(), Error> {
     let mut a = Pod::new_hash();
     let mut b = a.clone();
     assert_eq!(true, a == b);
-    match a.insert("hello", Pod::String("world".into())) {
-        Ok(_) => {}
-        Err(err) => panic!(err),
-    }
-    match b.insert("hello", Pod::String("world".into())) {
-        Ok(_) => {}
-        Err(err) => panic!(err),
-    }
+    a.insert("hello", Pod::String("world".into()))?;
+    b.insert("hello", Pod::String("world".into()))?;
     assert_eq!(true, a == b);
-    match a.insert("map", a.clone()) {
-        Ok(_) => {}
-        Err(err) => panic!(err),
-    }
-    match b.insert("map", b.clone()) {
-        Ok(_) => {}
-        Err(err) => panic!(err),
-    }
+    a.insert("map", a.clone())?;
+    b.insert("map", b.clone())?;
     assert_eq!(true, a == b);
-    match a.insert("boolean", Pod::Boolean(true)) {
-        Ok(_) => {}
-        Err(err) => panic!(err),
-    }
-    match b.insert("boolean", Pod::Boolean(false)) {
-        Ok(_) => {}
-        Err(err) => panic!(err),
-    }
+    a.insert("boolean", Pod::Boolean(true))?;
+    b.insert("boolean", Pod::Boolean(false))?;
     assert_eq!(false, a == b);
     assert_eq!(true, a.remove("boolean") == Pod::Boolean(true));
     assert_eq!(true, b.remove("boolean") == Pod::Boolean(false));
     assert_eq!(true, a == b);
-    match b.insert("hello", Pod::String("world!".into())) {
-        Ok(_) => {}
-        Err(err) => panic!(err),
-    }
+    b.insert("hello", Pod::String("world!".into()))?;
     assert_eq!(false, a == b);
+    Ok(())
 }
